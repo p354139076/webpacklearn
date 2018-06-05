e39c049271aa4013df7d8eb8a6aaf6fe2b55683e
p354139076 <p354139076@qq.com>
2018-06-05 16:21:34 +0800

    update 18/6/5

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
+const webpack=require('webpack');
+const HtmlWebpackPlugin=require('html-webpack-plugin')
 module.exports={
     entry:__dirname+"/app/main.js",
     mode: 'development',
     output:{
-        path:__dirname+"/public",
+        path:__dirname+"/build",
         filename:"bundel.js"
     },
     devServer:{
@@ -16,14 +18,31 @@ module.exports={
                 test: /(\.jsx|\.js)$/,
                 use: {
                     loader: "babel-loader",
-                    options: {
-                        presets: [
-                            "env", "react"
-                        ]
-                    }
                 },
                 exclude: /node_modules/
+            },
+            {
+                test:/\.css$/,
+                use:[
+                    {
+                    loader:"style-loader"   
+                    },
+                    {
+                        loader:"css-loader",         
+                        options:{
+                            module:true,
+                            localdentName:'[name]__[local]--[hash:base64:5]'
+                        }
+                    },
+                    {
+                        loader:"postcss-loader"
+                    }
+                ]
             }
         ]
-    }
+    },
+    plugins:[
+        new webpack.BannerPlugin('版权所有，翻版必究'),
+        new HtmlWebpackPlugin({template:__dirname+'/app/index.tmpl.html'})
+    ]
 };
